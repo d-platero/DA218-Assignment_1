fe66ae44573d57c5e46e4278fa4c98a9b5f5fce5
d-platero <131712816+d-platero@users.noreply.github.com>
2023-08-18 04:09:04 +0200

    Home and book view in place

--- js/main.js
+++ js/main.js
@@ -6,10 +6,10 @@ let books,
 async function start() {
   books = await getJSON('./public/data/books.json')
   console.log(books)
+  startPage()
   if (seeBooks) {
     viewBooks() 
   }
-  startPage()
 }
 
 function startPage() {
@@ -17,6 +17,15 @@ function startPage() {
   <p class="HomeTitle text-center">Home page</p>
   <p>Welcome! This is the home page for the book store. Use the navigation bar above to access what you want.</p>`;
   document.querySelector(".bookView").innerHTML = ``;
+}
+
+document.querySelector("#Home").onclick = function () {
+  document.querySelector(".mainView").innerHTML = `
+  <p class="HomeTitle text-center">Home page</p>
+  <p>Welcome! This is the home page for the book store. Use the navigation bar above to access what you want.</p>
+  `;
+
+  document.querySelector(".bookView").innerHTML = ``
 }
 
 function viewBooks() {
@@ -33,7 +42,15 @@ function viewBooks() {
   <p>${category}</p>
   <p>${price}</p>
   <button class="detailsBtn btn btn-primary></button
-  <button class="buyBtn btn btn-primary></button>`); // last two are details and purhcase buttons
+  <button class="buyBtn btn btn-primary></button>
+  </div>`); // last two are details and purhcase buttons
+}
+
+document.querySelector("#Books").onclick = function () {
+  if (!seeBooks) {
+    seeBooks == 1
+  }
+  start()
 }
 
 start();
