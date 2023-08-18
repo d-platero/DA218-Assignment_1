2eacd41948e5ec488264be9a40744ef2cd290249
d-platero <131712816+d-platero@users.noreply.github.com>
2023-08-18 20:44:59 +0200

    Add list of books to book page

--- js/main.js
+++ js/main.js
@@ -14,14 +14,14 @@ async function start() {
 
 function startPage() {
   document.querySelector(".mainView").innerHTML = `
-  <p class="HomeTitle text-center">Home page</p>
+  <p class="homeTitle text-center">Home page</p>
   <p>Welcome! This is the home page for the book store. Use the navigation bar above to access what you want.</p>`;
   document.querySelector(".bookView").innerHTML = ``;
 }
 
 document.querySelector("#Home").onclick = function () {
   document.querySelector(".mainView").innerHTML = `
-  <p class="HomeTitle text-center">Home page</p>
+  <p class="homeTitle text-center">Home page</p>
   <p>Welcome! This is the home page for the book store. Use the navigation bar above to access what you want.</p>
   `;
 
@@ -30,25 +30,23 @@ document.querySelector("#Home").onclick = function () {
 
 function viewBooks() {
   document.querySelector(".bookView").innerHTML = `
-  <p class = "BookViewTitle text-center">Book page</p>
-  <p></p>`
+  <p class = "bookViewTitle text-center">Book page</p>`
   document.querySelector(".mainView").innerHTML = ``;
   
-  document.querySelector(".bookView").innerHTML += books.map(({ bookTitle, bookAuthor, description, category, price }) => `
-  <div class="bookItem">
-  <p>${bookTitle}</p>
-  <p>${bookAuthor}</p>
-  <p>${description}</p>
-  <p>${category}</p>
-  <p>${price}</p>
-  <button class="detailsBtn btn btn-primary></button
-  <button class="buyBtn btn btn-primary></button>
-  </div>`); // last two are details and purhcase buttons
+  document.querySelector(".bookView").innerHTML += books.map(({ title, author, category, price }) => `
+  <div class="bookItem card mt-3 w-25" align="left">
+  <p><span>Title:</span> ${title}</p>
+  <p><span>Author:</span> ${author}</p>
+  <p><span>Category:</span> ${category}</p>
+  <p><span>Price:</span> ${price}</p>
+  <a class="detailsBtn btn btn-primary>Details</a>
+  <a class="buyBtn btn btn-primary></a>
+  </div>`).join(''); // last two are details and purhcase buttons
 }
 
 document.querySelector("#Books").onclick = function () {
   if (!seeBooks) {
-    seeBooks == 1
+    seeBooks++
   }
   start()
 }
