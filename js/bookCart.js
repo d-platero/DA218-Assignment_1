ff669999971a1b2b4badf75a31ed1b041fdb60e3
d-platero <131712816+d-platero@users.noreply.github.com>
2023-08-20 03:39:05 +0200

    Added cart functionality, buy button left

--- js/bookCart.js
+++ js/bookCart.js
@@ -1,4 +1,6 @@
 var bookCart = []
+var number = 0
+var total = 0
 
 // Use number to count how many books if its the same book
 function addBook(title, price, number) {
@@ -7,5 +9,23 @@ function addBook(title, price, number) {
     price: price,
     number: number
   }
+  if (!bookCart.some(elem => elem.title === title)) {
+    bookCart.push(book)
+  }
+  bookCart.filter((elem) => { return elem.title === title }).number++
+  
+  total += book.price;
+
+  document.querySelector(".price").innerHTML = `<div><p>${total}</p></div>`
+
+
+  var html = bookCart.map(({ title, price, number }) =>
+    `<div class="cartItem">
+    <p>Title: ${title}</p>
+    <p>Price: ${price}</p>
+    <p>Number: ${number}</p>
+    <p>Row sum: ${(price * number)} </p>`
+  )
+    document.querySelector('.cartItem').innerHTML = html.join('')
 }
 
